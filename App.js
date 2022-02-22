40d0fc0b36bc9b9cb17616e22e4cf1cd72fd4cce
Arthur57330 <arthur.trus1@gmail.com>
2022-02-22 23:19:09 +0100

    d

--- App.js
+++ App.js
@@ -66,6 +66,7 @@ function menu(){
     <ScrollView contentContainerStyle={{ flex: 1, backgroundColor: 'white', padding: 10, height: '100%', overflow: 'auto' }}>
       <Text>DelivCrous</Text>
       <View style={styles.container}>
+        
         <TextInput
           placeholder="Rechercher un plat..."
           style={styles.searchInput}
